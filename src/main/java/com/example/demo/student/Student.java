a29d1c582db57a6351d494371b5af84bf449623f
tonrugeria <tonrugeria@gmail.com>
2021-10-31 00:29:18 +0800

    FINAL COMMIT adding @transient & @transactional annotation saving, update, deleting student writing business logic testing POST Request

--- src/main/java/com/example/demo/student/Student.java
+++ src/main/java/com/example/demo/student/Student.java
@@ -2,6 +2,7 @@ package com.example.demo.student;
 
 import javax.persistence.*;
 import java.time.LocalDate;
+import java.time.Period;
 
 @Entity
 @Table
@@ -20,7 +21,8 @@ public class Student {
     private String name;
     private String email;
     private LocalDate dob;
-    private int age;
+    @Transient
+    private Integer age;
 
     public Student() {
     }
@@ -28,23 +30,19 @@ public class Student {
     public Student(Long id,
                    String name,
                    String email,
-                   LocalDate dob,
-                   int age) {
+                   LocalDate dob) {
         this.id = id;
         this.name = name;
         this.email = email;
         this.dob = dob;
-        this.age = age;
     }
 
     public Student(String name,
                    String email,
-                   LocalDate dob,
-                   int age) {
+                   LocalDate dob) {
         this.name = name;
         this.email = email;
         this.dob = dob;
-        this.age = age;
     }
 
     public Long getId() {
@@ -80,10 +78,10 @@ public class Student {
     }
 
     public int getAge() {
-        return age;
+        return Period.between(this.dob, LocalDate.now()).getYears();
     }
 
-    public void setAge(int age) {
+    public void setAge(Integer age) {
         this.age = age;
     }
 
---
+++ src/main/java/com/example/demo/student/StudentConfig.java
@@ -0,0 +1,36 @@
+package com.example.demo.student;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+import static java.time.Month.*;
+
+@Configuration
+public class StudentConfig {
+
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository repository) {
+        return args -> {
+            Student mariam = new Student(
+                    "Mariam",
+                    "Mariam.jamal@gmail.com",
+                    LocalDate.of(2000, JANUARY, 5)
+            );
+
+            Student alex = new Student(
+                    "Alex",
+                    "Alex@gmail.com",
+                    LocalDate.of(2004, JANUARY, 5)
+            );
+
+            repository.saveAll(
+                    List.of(mariam, alex)
+            );
+        };
+    }
+}
